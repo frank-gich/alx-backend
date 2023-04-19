d6bd5854b83c76858d32fbbeec8cd54b94f12e46
frank-gich <gichuhi5781@gmail.com>
2023-04-19 23:19:24 +0300

    my  lifo caching file

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -24,9 +24,9 @@ class LIFOCache(BaseCaching):
         else:
             length = len(self.cache_data)
             if length >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
-                print("DISCARD: {}".format(self.usage[1]))
-                del self.cache_data[self.usage[1]]
-                del self.usage[1]
+                print("DISCARD: {}".format(self.usage[-1]))
+                del self.cache_data[self.usage[-1]]
+                del self.usage[-1]
             if key in self.usage:
                 del self.usage[self.usage.index(key)]
             self.usage.append(key)
