b7c3357cc372c660670f3b3d73df87e719ffcbf8
frank-gich <gichuhi5781@gmail.com>
2023-04-19 23:38:14 +0300

    my basic caching fileand configuration file

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -12,15 +12,24 @@ class BasicCache(BaseCaching):
         """
         Initialize the init method
         """
-        BaseCaching.__init__(self)
+        super().__init__()
+        self.usage = []
 
     def put(self, key, item):
         """
-        Cache a key-value 
+        Cache a key-value
         """
         if key is None or item is None:
             pass
         else:
+            length = len(self.cache_data)
+            if length >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
+                print("DISCARD: {}".format(self.usage[0]))
+                del self.cache_data[self.usage[0]]
+                del self.usage[0]
+            if key in self.usage:
+                del self.usage[self.usage.index(key)]
+            self.usage.append(key)
             self.cache_data[key] = item
 
     def get(self, key):
@@ -28,5 +37,7 @@ class BasicCache(BaseCaching):
         Return none
         """
         if key is not None and key in self.cache_data.keys():
+            del self.usage[self.usage.index(key)]
+            self.usage.append(key)
             return self.cache_data[key]
         return None
