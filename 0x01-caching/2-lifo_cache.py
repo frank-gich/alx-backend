ea0ebda198569496d16c52d0aa70c0f5ce6b1f19
frank-gich <gichuhi5781@gmail.com>
2023-04-19 23:15:27 +0300

    my  lifo caching file

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -4,9 +4,9 @@
 from base_caching import BaseCaching
 
 
-class LRUCache(BaseCaching):
+class LIFOCache(BaseCaching):
     """
-    lru caching system
+    LIFOCache system
     """
     def __init__(self):
         """
@@ -24,9 +24,9 @@ class LRUCache(BaseCaching):
         else:
             length = len(self.cache_data)
             if length >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
-                print("DISCARD: {}".format(self.usage[0]))
-                del self.cache_data[self.usage[0]]
-                del self.usage[0]
+                print("DISCARD: {}".format(self.usage[1]))
+                del self.cache_data[self.usage[1]]
+                del self.usage[1]
             if key in self.usage:
                 del self.usage[self.usage.index(key)]
             self.usage.append(key)
