f9509afb399c0e48f74c7bf8ec06b86321916920
frank-gich <gichuhi5781@gmail.com>
2023-04-19 23:03:56 +0300

    my  lru caching file

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+""" BaseCaching module implimentation
+"""
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """
+   lru caching system
+    """
+
+    def __init__(self):
+        """
+        Initialize the init method
+        """
+        super().__init__()
+        self.usage = []
+
+    def put(self, key, item):
+        """
+        Cache a key
+        """
+        if key is None or item is None:
+            pass
+        else:
+            length = len(self.cache_data)
+            if length >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
+                print("DISCARD: {}".format(self.usage[-1]))
+                del self.cache_data[self.usage[-1]]
+                del self.usage[-1]
+            if key in self.usage:
+                del self.usage[self.usage.index(key)]
+            self.usage.append(key)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Return none
+        """
+        if key is not None and key in self.cache_data.keys():
+            del self.usage[self.usage.index(key)]
+            self.usage.append(key)
+            return self.cache_data[key]
+        return None
