4bf429fb72f9c713373b5155a5b63786ca4f7571
frank-gich <gichuhi5781@gmail.com>
2023-04-19 22:33:27 +0300

    my readme file and lfu caching

---
+++ 0x01-caching/100-lfu_cache.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+from base_caching import BaseCahing
+
+
+class LFUCache(BaseCahing):
+    """creates a LFUCache system without limit
+    methords:
+        put (key, item): add a key/value pair to cache
+        get(key): gets the key from cache
+    """
+    
+    def __init__(self) -> None:
+        """ init object"""
+        super().__init__()
+        self.uses = dict()
+        
+    def put(self, key, item):
+        """adding an item in the cache"""
+        if(key is None or item is None):
+            return
+        
+        if(len(self.cache_data.keys()) == BaseCahing.MAX_ITEMS
+           and key not in self.cache_data.keys()):
+            discard_key = min(self.uses, key=self.uses.get)
+            del self.cache_data[discard_key]
+            del self.uses[discard_key]
+            print("DISCARD: {}". format(discard_key))
+        if(key in self.cache_data.keys()):
+            self.uses[key] += 1
+        else:
+            self.uses[key] = 1
+        self.cache_data[key] = item
+        
+    def get(self, key) :
+        """geting an item"""
+        if ( key is None or key  not in self.cache.data.keys()):
+           return
+        self.uses[key] += 1
+        return self.cache_data.get(key)
